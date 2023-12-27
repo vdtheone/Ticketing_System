53710657d22c0716ebcc8faee60ebdf9805b0805
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-27 14:52:13 +0530

    create one route to show attachment when debug is false

--- ticketing_system/ticketing_system/urls.py
+++ ticketing_system/ticketing_system/urls.py
@@ -20,8 +20,17 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import re_path
+from django.views.static import serve
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls"))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
